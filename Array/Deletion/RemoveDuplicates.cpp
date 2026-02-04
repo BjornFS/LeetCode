2615cb4ebdf28ed5863aea5312d867c03f33c0ee
Bjørn Funch Schrøder <Bjornfschroder@gmail.com>
2026-02-04 23:16:30 +0100

    Arrays

--- Array/Deletion/RemoveDuplicates.cpp
+++ Array/Deletion/RemoveDuplicates.cpp
@@ -9,7 +9,8 @@ int removeDuplicates(vector<int>& nums) {
     int writeIdx = 1;    // first element is always kept
     for (std::size_t readIdx = 1; readIdx < nums.size(); ++readIdx) {
         if (nums[readIdx] != nums[readIdx - 1]) {
-            nums[writeIdx++] = nums[readIdx];
+            nums[writeIdx] = nums[readIdx];
+            writeIdx++;
         }
     }
     return writeIdx;
