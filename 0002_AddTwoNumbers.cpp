0eb1715f52e0b197282f03775b1840cd17e59527
Bjørn Funch Schrøder <Bjornfschroder@gmail.com>
2026-02-10 18:24:10 +0100

    daily

---
+++ 0002_AddTwoNumbers.cpp
@@ -0,0 +1,34 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+    ListNode* dummyHead = new ListNode(0);
+    ListNode* current = dummyHead;
+    int carry = 0;
+
+    while (l1 != nullptr || l2 != nullptr) {
+        int val1 = (l1 != nullptr) ? l1->val : 0;
+        int val2 = (l2 != nullptr) ? l2->val : 0;
+
+        int total = val1 + val2 + carry;
+        carry = total / 10;
+
+        current->next = new ListNode(total % 10);
+        current = current->next;
+
+        if (l1 != nullptr) l1 = l1->next;
+        if (l2 != nullptr) l2 = l2->next;
+    }
+
+    if (carry > 0) {
+        current->next = new ListNode(carry);
+    }
+
+    return dummyHead->next;
+};
